001f4874bb5859c8e7dd66edb65da113170ae73a
pabon98 <hasinanjumpabon@gmail.com>
2022-01-10 21:51:22 +0600

    all added

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName('btn').style.color="white";
+document.getElementsByClassName('btn').style.backgroundcolor="blueviolet"
+document.getElementsByClassName('btn').style.fontsize="20px"
+document.getElementsByClassName('btn').style.width="150px"
+document.getElementsByClassName('btn').style.height="60px"
+document.getElementsByClassName('btn').style.border="1px solid blueviolet"
+document.getElementsByClassName('btn').style.borderradius="15px"
